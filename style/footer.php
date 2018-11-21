fd788b1bc8ac36d43c2dbbcf49f9bc67679122db
Quinn Heagy (Lyfa) <quinn@quinnheagy.com>
2018-11-21 20:49:05 +0900

    I can't make decisions

--- style/footer.php
+++ style/footer.php
@@ -6,8 +6,8 @@ $path = pathinfo($url);
 			<footer class="mastfoot mt-auto">
 				<div class="inner">
 					<nav class="nav nav-masthead justify-content-center">
-						<a class="nav-link" href="https://quinnheagy.com/" target="_blank">Me</a>
-						<a class="nav-link" href="https://github.com/QHeagy/chatbox" target="_blank">GitHub</a>
+						<!--<a class="nav-link" href="https://quinnheagy.com/" target="_blank">Lyfa</a>
+						<a class="nav-link" href="https://github.com/QHeagy/chatbox" target="_blank">GitHub</a>-->
 						<? if($_SESSION['logged_in'] != true) { ?> 
 						<a class="nav-link" href="signup.php">Sign Up</a>
 						<a class="nav-link" href="login.php">Sign In</a>
