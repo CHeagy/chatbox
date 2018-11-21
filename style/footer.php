c5b7437aafa1ce480a5974e7224fdfad94570a09
Quinn Heagy (Lyfa) <quinn@quinnheagy.com>
2018-11-21 17:36:19 +0900

    Minor tweaks

--- style/footer.php
+++ style/footer.php
@@ -1,7 +1,6 @@
-<!--<pre><?=var_dump($_SERVER, true)?></pre>-->
 <?php
 $url = "http" . (($_SERVER['SERVER_PORT'] == 443) ? "s" : "") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$r = pathinfo($url);
+$path = pathinfo($url);
 ?>
 
 			<footer class="mastfoot mt-auto">
@@ -13,7 +12,7 @@ $r = pathinfo($url);
 						<a class="nav-link" href="signup.php">Sign Up</a>
 						<a class="nav-link" href="login.php">Sign In</a>
 						<? } else if($_SESSION['logged_in'] == true) { ?>
-							<? if($r['filename'] == "account") { ?>
+							<? if($path['filename'] == "account") { ?>
 								<a class="nav-link" href="index.php">Home</a>
 							<? } else { ?>
 								<a class="nav-link" href="account.php">My Account</a>
