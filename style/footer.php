e0554e5b20c93b8df48437ebebfcea39d8ad7865
Quinn Heagy (Lyfa) <quinn@quinnheagy.com>
2018-11-21 21:32:59 +0900

    Minor footer changes because fuck it

--- style/footer.php
+++ style/footer.php
@@ -8,16 +8,13 @@ $path = pathinfo($url);
 					<nav class="nav nav-masthead justify-content-center">
 						<!--<a class="nav-link" href="https://quinnheagy.com/" target="_blank">Lyfa</a>
 						<a class="nav-link" href="https://github.com/QHeagy/chatbox" target="_blank">GitHub</a>-->
+						<a class="nav-link" href="index.php">Home</a>
 						<? if($_SESSION['logged_in'] != true) { ?> 
 						<a class="nav-link" href="signup.php">Sign Up</a>
 						<a class="nav-link" href="login.php">Sign In</a>
 						<? } else if($_SESSION['logged_in'] == true) { ?>
-							<? if($path['filename'] == "account") { ?>
-								<a class="nav-link" href="index.php">Home</a>
-							<? } else { ?>
-								<a class="nav-link" href="account.php">My Account</a>
-							<? } ?>
-						<a class="nav-link" href="logout.php">Sign Out</a>
+							<a class="nav-link" href="account.php">My Account</a>
+							<a class="nav-link" href="logout.php">Sign Out</a>
 						<? } ?>
 					</nav>
 				</div>
